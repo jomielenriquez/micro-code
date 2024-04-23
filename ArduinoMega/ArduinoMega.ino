c20620f2c67c8122bc5ad5ceecada1871d9c7dcd
Jomiel <enriquez.jliquigan@gmail.com>
2024-04-23 22:31:10 +0800

    Update

--- ArduinoMega/ArduinoMega.ino
+++ ArduinoMega/ArduinoMega.ino
@@ -20,14 +20,21 @@ HX711 scale;
 float calibration_factor = -284; //-7050 worked for my 440lb max scale setup
 float units;
 
+// dogWait is the duration of time that the system will wait for the dog
+// if the dog was not detected after dogWait, it will be considered as fail dispense
+int dogWait = 10000;
+
+int statusCount = 0;
+int statusCountLimit = 5000; // update every 5 sec
+
 Servo servo;
 SoftwareSerial esp8266(10,11);
 
 String serialMessage = "";
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
-  esp8266.begin(9600);
+  Serial.begin(9600);
+  esp8266.begin(19200);
   Serial.println("Initializing");
 
   servo.attach(4);
@@ -53,27 +60,37 @@ void setup() {
 void loop() {
   bool isOpen = false;
   
-  /* 
-  Create a variable that will hold the next schedule
-  when the dog is need, check the next schedule
-  */
   float dogDistance = getDogDistance();
   Serial.print("Dog Distance: ");
-  Serial.println(dogDistance);
-  if(dogDistance < 5){
-    isOpen = true;
-    serialMessage = "{\"message\": \"Detected.\"}";
-  }
-
-  Serial.print("Food Distance: ");
-  Serial.println(getFoodDistance());
+  Serial.print(dogDistance);
+  
+  Serial.print(", Food Distance: ");
+  Serial.print(getFoodDistance());
 
   // zero means there is water
-  Serial.print("Water Reading: ");
+  Serial.print(", Water Reading: ");
   Serial.println(readWaterSensor());
+
+  if(statusCount >= statusCountLimit){
+    Serial.print("passing status");
+    Serial.print("{\"message\": \"UPDATE\",\"amount\": 0,\"water\":");
+    Serial.print(readWaterSensor());
+    Serial.print(",\"food\":");
+    Serial.print(getFoodDistance());
+    Serial.println("}");
+    // below is the sample message to update food and water status
+    // "{\"message\": \"UPDATE\",\"amount\": 0,\"water\": 0,\"food\": 0}"
+    esp8266.print("{\"message\": \"UPDATE\",\"amount\": 0,\"water\":");
+    esp8266.print(readWaterSensor());
+    esp8266.print(",\"food\":");
+    esp8266.print(getFoodDistance());
+    esp8266.println("}");
+    statusCount = 0;
+  }
 
   while(esp8266.available()){
     char c = (char)esp8266.read();
+    Serial.print(c);
     if(c == '\n'){
       isOpen = true;
     }
@@ -88,28 +105,90 @@ void loop() {
 
     deserializeJson(doc, serialMessage);
     String docMessage = doc["message"];
-    if(docMessage != "null"){
+    String from = doc["from"];
+    String to = doc["to"];
+    if(docMessage != "null" && from.toInt() > 0 && to.toInt() > 0){
+      Serial.println(from.toInt());
+
       Serial.println("Message: '" + docMessage + "'");
       
       Serial.print("Reading: "); 
       Serial.print(getWeight());
       Serial.println(" grams");
 
-      servo.write(0); // move MG996R's shaft to angle 180°
-      delay(1000); // wait for one second
-      servo.write(90);
-      delay(1000); // wait for one second
-      servo.write(0); // move MG996R's shaft to angle 180°
-      delay(1000); // wait for one second
-      servo.write(90);
-      delay(1000); // wait for one second
+      bool isDone = false;
+
+      // after the schedule is sent from esp it will wait for the dog to come near the utrasonic sensor
+      int currentWaitingTime = 0;
+      Serial.print("Waiting for the dog");
+      while(currentWaitingTime < dogWait){
+        dogDistance = getDogDistance();
+        if(dogDistance < 7){
+          break;
+        }
+        Serial.print(".");
+        delay(1000);
+        currentWaitingTime += 1000;
+      }
+      Serial.println(". Dog detected");
+
+      if(dogDistance < 7){
+        while((int)getWeight() < from.toInt()){
+          
+          Serial.print("Reading: "); 
+          Serial.print(getWeight());
+          Serial.println(" grams");
+
+          servo.write(0);
+          delay(1000);
+          servo.write(90);
+          delay(1000);
+
+          isDone = true;
+        }
+        if(isDone){
+          int amount = (int)getWeight();
+          esp8266.print("{\"message\": \"POST\",\"amount\": ");
+          esp8266.print(amount);
+          esp8266.println(",\"remarks\": \"ok\"}");
+        }
+        else{
+          // food is still full
+          // feeder did not detect the dog.
+          int scheduledAmount = from.toInt() * -1;
+          esp8266.print("{\"message\": \"POST\",\"amount\": ");
+          esp8266.print(scheduledAmount);
+          esp8266.print(",\"remarks\": \" Did not dispense at ");
+          esp8266.print(docMessage);
+          esp8266.print(" (");
+          esp8266.print(from);
+          esp8266.print(") because current weight is ");
+          esp8266.print((int)getWeight());
+          esp8266.print(" grams");
+          esp8266.println("\"}");
+        }
+      }
+      else{
+        // feeder did not detect the dog.
+        int scheduledAmount = from.toInt() * -1;
+        esp8266.print("{\"message\": \"POST\",\"amount\": ");
+        esp8266.print(scheduledAmount);
+        esp8266.print(",\"remarks\": \" Did not dispense at ");
+        esp8266.print(docMessage);
+        esp8266.println(" because dog was not detected.\"}");
+      }
+      Serial.println("Dispense Done");
+      // delay 60000 or 1 minute after dispense process
+      delay(60000);
     }
     else{
       Serial.println("Message: '" + serialMessage + "'");
     }
-
+    finished:
     serialMessage = "";
   }
+  delay(1000);
+  statusCount = statusCount + 1000;
 }
 
 int readWaterSensor(){
--- ESP8266Code/ESP8266Code.ino
+++ ESP8266Code/ESP8266Code.ino
@@ -6,13 +6,26 @@
 #include <LittleFS.h> // Include LittleFS library instead of SPIFFS
 #include <Wire.h>
 #include <ArduinoJson.h>
+#include <RTClib.h>
 
-// Replace with your network credentials
+RTC_DS3231 rtc;
+
+char t[32]; // time holder
+StaticJsonDocument<200> schedulingJson;
+
+// Wifi Credentials
 const char* ssid = "Last_Warning_2.4G";
 const char* password = "Wisbagslots121625";
 
+// Host of the data
+const char* host = "dataapi.somee.com";
+const int port = 80; // Use port 80 for HTTP
+
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
+
+float foodDistance = 6;
+int water = 0; // 0 means there is water and 1 means no water
 
 void overwriteJsonFile(const char* filename, String datastring) {
   // Open the file for writing
@@ -28,12 +41,12 @@ void overwriteJsonFile(const char* filename, String datastring) {
   // Close the file
   file.close();
 
-  Serial.println("File updated successfully");
+  // Serial.println("File updated successfully");
 }
 
 void setup() {
   // Serial port for debugging purposes
-  Serial.begin(9600);
+  Serial.begin(19200);
 
   // Initialize LittleFS
   if (!LittleFS.begin()) { // Use LittleFS instead of SPIFFS
@@ -41,11 +54,11 @@ void setup() {
     return;
   }
 
-  IPAddress ip(192, 168, 1, 10); // Desired fixed IP address
-  IPAddress gateway(192, 168, 1, 1);
-  IPAddress subnet(255, 255, 255, 0);
+  // IPAddress ip(192, 168, 1, 10); // Desired fixed IP address
+  // IPAddress gateway(192, 168, 1, 1);
+  // IPAddress subnet(255, 255, 255, 0);
 
-  WiFi.config(ip, gateway, subnet);
+  // WiFi.config(ip, gateway, subnet);
   
   // Connect to Wi-Fi
   WiFi.begin(ssid, password);
@@ -90,8 +103,8 @@ void setup() {
 
     String jsonString;
     serializeJson(jsonDocument, jsonString);
-    Serial.printf(jsonString.c_str());
-    Serial.println();
+    // Serial.printf(jsonString.c_str());
+    // Serial.println();
     request->send(200, "application/json", jsonString);
   });
 
@@ -112,8 +125,31 @@ void setup() {
     }
 
     String jsonString;
+    schedulingJson = jsonDocument;
     serializeJson(jsonDocument, jsonString);
-    Serial.println(jsonString);
+    // Serial.println(jsonString);
+    request->send(200, "application/json", jsonString);
+  });
+
+  server.on("/NotifData", HTTP_GET, [](AsyncWebServerRequest* request) {
+    File file = LittleFS.open("/notifData.json", "r");
+    if (!file) {
+      request->send(404, "text/plain", "File not found");
+      return;
+    }
+
+    StaticJsonDocument<200> jsonDocument;
+    DeserializationError error = deserializeJson(jsonDocument, file);
+    file.close();
+
+    if (error) {
+      request->send(500, "text/plain", "Failed to parse JSON");
+      return;
+    }
+
+    String jsonString;
+    serializeJson(jsonDocument, jsonString);
+    // Serial.println(jsonString);
     request->send(200, "application/json", jsonString);
   });
 
@@ -131,12 +167,22 @@ void setup() {
     request->send(200, "application/json", "[{\"status\":\"success\"}]");
   });
 
+  server.on("/getupdates", HTTP_GET, [](AsyncWebServerRequest* request) {
+    String updates = "[{\"water\":" + String(water) + ", \"food\":" + String(foodDistance) + "}]";
+    request->send(200, "application/json", updates);
+  });
+
   // Route for handling POST requests
   server.on("/postSchedule", HTTP_POST, [](AsyncWebServerRequest* request) {
     if (request->hasParam("data", true)) {
       AsyncWebParameter* dataParam = request->getParam("data", true);
       String datastring = dataParam->value();
+
+      StaticJsonDocument<200> jsonDocument;
+      DeserializationError error = deserializeJson(jsonDocument, datastring);
       
+      schedulingJson = jsonDocument;
+
       overwriteJsonFile("/schedulingData.json", datastring);
     } else {
       Serial.println("No datetimeInput parameter found");
@@ -147,7 +193,165 @@ void setup() {
 
   // Start server
   server.begin();
+
+  rtc.begin();
+
+  // if you need to set the time, use rtc.adjust.
+  // Below is an example of setting the time to April 22, 2024 - 9:39 PM
+  // rtc.adjust(DateTime(2024, 4, 22, 21, 39, 0)); 
+  DateTime now = rtc.now();  // Get initial time from RTC
+
+  File file = LittleFS.open("/schedulingData.json", "r");
+  if (file) {
+    StaticJsonDocument<200> jsonDocument;
+    DeserializationError error = deserializeJson(jsonDocument, file);
+    file.close();
+
+    if (!error) {
+      schedulingJson = jsonDocument;
+    }
+  }
 }
 
 void loop() {
+  if(Serial.available()){
+    String message = Serial.readStringUntil('\n');
+    DynamicJsonDocument serialMessage(1024);
+
+    deserializeJson(serialMessage, message);
+    //"{\"message\": \"POST\",\"amount\": 10}"
+    if(serialMessage["message"] == "POST"){
+      if(serialMessage["remarks"] != "ok"){
+        insertNotification("Sched", serialMessage["remarks"]);
+      }
+      postDispense(serialMessage["amount"]);
+      delay(60000);
+    }
+    else if(serialMessage["message"] == "UPDATE" && serialMessage["water"] >= 0 && serialMessage["food"] >= 0){
+      // Update water and food distance
+      water = serialMessage["water"];
+      foodDistance = serialMessage["food"];
+    }
+  }
+  // Handle outgoing connections
+  // handleOutgoingConnections();
+
+  DateTime rtcTime = rtc.now();
+  // Format and print RTC time on Serial monitor
+  sprintf(t, "RTC Time: %02d:%02d:%02d %02d/%02d/%02d", rtcTime.hour(), rtcTime.minute(), rtcTime.second(), rtcTime.day(), rtcTime.month(), rtcTime.year());
+  // Serial.println(t);
+
+  char currentTime[5];
+  sprintf(currentTime, "%02d:%02d", rtcTime.hour(), rtcTime.minute());
+  String stringCurrentTime = String(currentTime);
+
+  String jsonString;
+  serializeJson(schedulingJson, jsonString);
+  // Serial.println(jsonString);
+
+  DynamicJsonDocument doc(1024);
+  deserializeJson(doc, jsonString);
+
+  // Count the number of elements in the document
+  int count = doc.size();
+  for(int row = 0; row < count; row++){
+    JsonObject object = doc[row];
+    const char* timeValue = object["time"];
+    String timeString = String(timeValue);
+    // Serial.println(timeString);
+    if(stringCurrentTime == timeString){
+      Serial.println("{\"message\": \"Dispense Time: "+timeString+"\",\"from\": \""+String(object["amountFrom"])+"\",\"to\": \""+String(object["amountTo"])+"\"}");
+    }
+  }
+
+  delay(1000); // Allow time for handling events
+}
+void handleOutgoingConnections() {
+  // Create a WiFiClient object
+  WiFiClient client;
+  
+  if (client.connect(host, port)) {
+    Serial.println("Connected to server");
+
+    // Send a simple HTTP GET request
+    client.print("GET /api/dispense/ HTTP/1.1\r\n");
+    client.print("Host: ");
+    client.print(host);
+    client.print("\r\n");
+    client.print("Connection: close\r\n\r\n");
+
+    // Read response
+    while (client.connected()) {
+      if (client.available()) {
+        String line = client.readStringUntil('\n');
+        Serial.println(line);
+      }
+    }
+  } else {
+    Serial.println("Connection failed");
+  }
+
+  // Close connection
+  client.stop();
+  Serial.println("Connection closed");
+}
+void postDispense(int amount) {
+  // Create a WiFiClient object
+  WiFiClient client;
+
+  // Connect to the host on the specified port
+  if (client.connect(host, port)) {
+    Serial.println("Connected to server");
+
+    String postData = "{ \"amount\": " + String(amount) + ", \"unit\":\"grams\", \"type\":\"food\" }"; // Your POST data
+
+    // Send a simple HTTP GET request
+    client.print("POST /api/dispense/ HTTP/1.1\r\n");
+    client.print("Host: ");
+    client.print(host);
+    client.print("\r\n");
+    client.print("Content-Type: application/json\r\n");
+    client.print("Content-Length: ");
+    client.print(postData.length());
+    client.print("\r\n\r\n");
+    client.print(postData);
+    client.print("Connection: close\r\n\r\n");
+
+    // Read response
+    while (client.connected()) {
+      if (client.available()) {
+        String line = client.readStringUntil('\n');
+        Serial.println(line);
+      }
+    }
+  } else {
+    Serial.println("Connection failed");
+  }
+
+  // Close connection
+  client.stop();
+  Serial.println("Connection closed");
+}
+void insertNotification(String type, String message){
+  File file = LittleFS.open("/notifData.json", "r");
+  
+  if (file) {
+    DynamicJsonDocument doc(500);
+    DeserializationError error = deserializeJson(doc, file);
+
+    file.close();
+
+    if (!error) {
+      JsonObject newObj = doc.createNestedObject();
+      newObj["Type"] = type;
+      newObj["Message"] = message;
+      
+      String jsonString;
+      serializeJson(doc, jsonString);
+
+      overwriteJsonFile("/notifData.json", jsonString);
+    }
+  }
+
+  file.close();
 }
